abb266f55acc5e841538a5a13e1639b04935ea7d
AfrojaSultana44 <afrojasultanamoni311@gmail.com>
2024-09-24 03:28:38 +0600

    Donate calculation , modal section completed

--- js/features.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("history-btn").addEventListener("click", function () {
-  showSection("history-section");
-});
-
-document.getElementById("donation-btn").addEventListener("click", function () {
-  showSection("card-section");
-});
--- js/index.js
+++ js/index.js
@@ -1,31 +1,108 @@
-document.getElementById("donateBtn").addEventListener("click", function () {
-  const donateAmount = getInputValueById("donate-amount");
+function donateHandler(id) {
+  let mainBalance = document.getElementById(`main-balance`).innerText;
+  let donateBalance = document.getElementById(`${id}-donate-balance`).innerText;
+  let donateAmount = document.getElementById(`${id}-donate-amount`).value;
 
-  const donateBalance = getTextFieldValueById("donate-balance");
+  let mainBalanceToNumber = Number(mainBalance);
+  let donateBalanceToNumber = Number(donateBalance);
+  let donateAmountToNumber = Number(donateAmount);
 
-  // const mainBalance = getTextFieldValueById("main-Balance");
+  if (
+    !isNaN(donateAmountToNumber) &&
+    donateAmountToNumber > 0 &&
+    mainBalanceToNumber >= donateAmountToNumber
+  ) {
+    donateBalance = donateBalanceToNumber + donateAmountToNumber;
 
-  if (donateAmount > 0 && !isNaN(donateAmount)) {
-    const newDonateBalance = donateBalance + donateAmount;
+    mainBalance = mainBalance - donateAmountToNumber;
 
-    document.getElementById("donate-balance").innerText = newDonateBalance;
+    document.getElementById(`${id}-donate-balance`).innerText = donateBalance;
 
-    // if(newDonateBalance){
+    document.getElementById(`main-balance`).innerText = mainBalance;
 
-    //   const newMainBalance = mainBalance - newDonateBalance;
+    document.getElementById(`${id}-donate-amount`).value = "";
 
-    //   document.getElementById("main-balance").innerText = newMainBalance;
-
-    //   console.log(newMainBalance);
-    // }
-
-    // add to donation history
     const div = document.createElement("div");
-    div.innerHTML = `
-    <p class="border rounded-2xl p-8 text-xl font-bold text-[#111111">${newDonateBalance} Taka is Donated for famine-2024 at Feni, Bangladesh</p>
+    let historyMessage = "";
+
+    if (id === "card-one") {
+      historyMessage = `
+      <div class="border rounded-2xl p-8 ">
+      <p class="font-bold text-xl text-[#111111]">${donateAmountToNumber} Taka is Donated for Flood at Noakhali, Bangladesh</p>
+      <p class="text-[16px] font-light text-[rgba(17,17,17,0.7)] mt-1">${new Date()}</p>
+      </div>
+      `;
+    } else if (id === "card-two") {
+      historyMessage = `
+      <div class="border rounded-2xl p-8 ">
+      <p class="font-bold text-xl text-[#111111]">${donateAmountToNumber} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+      <p class="text-[16px] font-light text-[rgba(17,17,17,0.7)] mt-1">${new Date()}</p>
+      </div>
+      `;
+    } else {
+      historyMessage = `
+    <div class="border rounded-2xl p-8 ">
+    <p class="font-bold text-xl text-[#111111]">${donateAmountToNumber} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
+    <p class="text-[16px] font-light text-[rgba(17,17,17,0.7)] mt-1">${new Date()}</p>
+    </div>
     `;
+    }
+
+    div.innerHTML = historyMessage;
+
     document.getElementById("history-section").appendChild(div);
+
+    document.getElementById("donation-success-modal").showModal();
   } else {
-    alert("Failed to donate money.");
+    alert(
+      "Invalid data or you have insufficient amount to donate. Please provide valid data"
+    );
   }
+}
+
+document
+  .getElementById("card-one-donate-btn")
+  .addEventListener("click", function () {
+    donateHandler("card-one");
+  });
+
+document
+  .getElementById("card-two-donate-btn")
+  .addEventListener("click", function () {
+    donateHandler("card-two");
+  });
+
+document
+  .getElementById("card-three-donate-btn")
+  .addEventListener("click", function () {
+    donateHandler("card-three");
+  });
+
+function showSection(id) {
+  // hide all the section
+  document.getElementById("card-section").classList.add("hidden");
+  document.getElementById("history-section").classList.add("hidden");
+
+  // show the section with provided id
+  document.getElementById(id).classList.remove("hidden");
+}
+
+const donationBtn = document.getElementById("donation-btn");
+
+const historyBtn = document.getElementById("history-btn");
+
+document.getElementById("donation-btn").addEventListener("click", function () {
+  showSection("card-section");
+  donationBtn.style.cssText =
+    "background-color: #B4F461; color: #111111; font-weight: 600;";
+  historyBtn.style.cssText =
+    "background-color: #ffffff; color: red; font-weight: 500;";
+});
+
+document.getElementById("history-btn").addEventListener("click", function () {
+  showSection("history-section");
+  historyBtn.style.cssText =
+    "background-color: #B4F461; color: #111111; font-weight: 600;";
+  donationBtn.style.cssText =
+    "background-color: #ffffff; color: red;  font-weight: 500;";
 });
